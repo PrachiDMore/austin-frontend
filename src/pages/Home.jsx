63c6332ff19101552e7db23cd8a13ba975d5b553
Prachi More <prachimore2702@gmail.com>
2023-04-29 15:43:24 +0530

    Home page created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
 	return (
 		<>
-			
+			<Link to='/signin'>Sign in</Link>
+			<Link to='/signup'>Sign up</Link>
 		</>
 	)
 }
