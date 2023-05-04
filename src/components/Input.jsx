fb73ebd3f9a425ae7a5e060dd1b483ce78a0d546
Prachi More <prachimore2702@gmail.com>
2023-05-04 21:41:43 +0530

    View Admin page

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,12 +3,12 @@ import { useState } from 'react'
 import { AiOutlineEyeInvisible, AiOutlineEye } from 'react-icons/ai'
 
 
-const Input = ({ id, label, placeholder, onChange, value, type, password = false, required = false }) => {
+const Input = ({ id, label = false, placeholder, onChange, value, type, password = false, required = false }) => {
   const [showPassword, setShowPassword] = useState(!password);
   return (
     <>
       <div className='flex flex-col Nunito relative'>
-        <label className='text-sm cursor-pointer mb-1 font-bold' htmlFor={id}>{label}:</label>
+        {label ? <label className='text-sm cursor-pointer mb-1 font-bold' htmlFor={id}>{label}:</label> : <p></p>}
         {password ? <input required={required} onChange={onChange} value={value} className='border focus:shadow-purpleShadow duration-300 outline-none shadow-md px-4 py-3 rounded-md w-full ' id={id} type={showPassword ? "text" : "password"} placeholder={placeholder} /> : <input onChange={onChange} value={value} className='border focus:shadow-purpleShadow duration-300 outline-none shadow-md px-4 py-3 rounded-md w-full ' id={id} type={type} placeholder={placeholder} required={required} />}
         <div onClick={() => { setShowPassword(!showPassword) }} className={password ? 'stroke-2 text-lg cursor-pointer absolute right-5 bottom-[14px] text-purpleBlue block' : 'hidden'}>
           {showPassword ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
--- src/pages/ViewAdmissions.jsx
+++ src/pages/ViewAdmissions.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Input from '../components/Input'
+import { GrSearch } from 'react-icons/gr'
 
 const ViewAdmissions = () => {
   return (
-	<div>
-	  
-	</div>
+    <div>
+      <Navbar />
+      <section className='w-screen min-h-screen p-10 px-20 Nunito'>
+        <div className='w-full '>
+          <Input type={'text'} placeholder={'Search'} />
+          <GrSearch className='text-lg font-bold relative bottom-8 left-[97%]' />
+        </div>
+        <ul className='flex justify-around border font-semibold text-darkPurple text-lg border-darkPurple py-2 rounded-md mt-4'>
+          <li>Name</li>
+          <li>Grade</li>
+          <li>Email</li>
+          <li>Phone Number</li>
+          <li>Gender</li>
+        </ul>
+        <div className='flex flex-col justify-around border border-darkPurple py-2 rounded-md mt-4 '>
+          <ul className='flex justify-around py-2 '>
+            <li>Asdfgh</li>
+            <li>10th</li>
+            <li>abcd</li>
+            <li>12345</li>
+            <li>Female</li>
+          </ul>
+
+        </div>
+      </section>
+    </div>
   )
 }
 
