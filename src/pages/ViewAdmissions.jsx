e77b72a150f989e3a0f12f95c82c83f1a41955d7
Mrudul Kolambe <mrudulkolambe02@gmail.com>
2023-05-05 21:30:44 +0530

    api integrated

--- src/pages/ViewAdmissions.jsx
+++ src/pages/ViewAdmissions.jsx
@@ -1,34 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import Input from '../components/Input'
 import { GrSearch } from 'react-icons/gr'
+import axios from 'axios'
 
 const ViewAdmissions = () => {
+  const [admissions, setAdmissions] = useState([]);
+  const [searchAdmissions, setSearchAdmissions] = useState([]);
+
+  useEffect(() => {
+    axios(`${process.env.REACT_APP_BASE_URL}/admission/getAllAdmissions`)
+      .then((res) => {
+        if (res.data.error) {
+          alert(res.data.message)
+        } else {
+          setSearchAdmissions(res.data.admissions)
+          setAdmissions(res.data.admissions)
+        }
+      })
+      .catch((err) => {
+        alert(err.message)
+      })
+  }, []);
+
+  const handleSearch = (e) => {
+    if (e.target.value.length == 0) {
+      setSearchAdmissions(admissions)
+    }else{
+      setSearchAdmissions(admissions.filter((data) => {
+        return `${data.firstname} ${data.lastname}`.toLowerCase().includes(e?.target?.value?.toLowerCase()) || data?.gender?.toLowerCase()?.includes(e.target.value.toLowerCase()) || data?.email?.toLowerCase()?.includes(e.target.value.toLowerCase()) || data?.mobileNoPrimary?.toLowerCase()?.includes(e.target.value.toLowerCase()) || data?.grade?.toLowerCase()?.includes(e.target.value.toLowerCase()) 
+      }))
+    }
+  }
   return (
     <div>
       <Navbar />
       <section className='w-screen min-h-screen p-10 px-20 Nunito'>
         <div className='w-full '>
-          <Input type={'text'} placeholder={'Search'} />
+          <Input onChange={handleSearch} type={'text'} placeholder={'Search...'} />
           <GrSearch className='text-lg font-bold relative bottom-8 left-[97%]' />
         </div>
-        <ul className='flex justify-around border font-semibold text-darkPurple text-lg border-darkPurple py-2 rounded-md mt-4'>
-          <li>Name</li>
-          <li>Grade</li>
-          <li>Email</li>
-          <li>Phone Number</li>
-          <li>Gender</li>
-        </ul>
-        <div className='flex flex-col justify-around border border-darkPurple py-2 rounded-md mt-4 '>
-          <ul className='flex justify-around py-2 '>
-            <li>Asdfgh</li>
-            <li>10th</li>
-            <li>abcd</li>
-            <li>12345</li>
-            <li>Female</li>
-          </ul>
-
+        <div class="mx-auto">
+          <div class="bg-white relative shadow-md shadow-purpleShadow rounded-lg overflow-hidden">
+            <div class="overflow-x-auto">
+              <table class="w-full text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-sm uppercase bg-darkPurple text-white">
+                  <tr>
+                    <th scope="col" class="px-6 py-5">Name</th>
+                    <th scope="col" class="px-6 py-5">Grade</th>
+                    <th scope="col" class="px-6 py-5">Email</th>
+                    <th scope="col" class="px-6 py-5">Phone Number</th>
+                    <th scope="col" class="px-6 py-5">Gender</th>
+                  </tr>
+                </thead>
+                <tbody className='text-gray-700 mt-5'>
+                  {
+                    searchAdmissions?.map((data) => {
+                      return <tr key={data?._id} class="border-b border-darkPurple">
+                        <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">{data?.firstname} {data?.lastname}</th>
+                        <td class="px-6 py-4">{data?.grade}</td>
+                        <td class="px-6 py-4">{data?.email}</td>
+                        <td class="px-6 py-4">{data?.mobileNoPrimary}</td>
+                        <td class="px-6 py-4 capitalize">{data?.gender}</td>
+                      </tr>
+                    })
+                  }
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
+
+
       </section>
     </div>
   )
